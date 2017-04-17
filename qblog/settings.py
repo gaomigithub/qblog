7e43e9dec36eea526e9d45a3c46ee8c754f68f46
GaoMi <gaomibuy@hotmail.com>
2017-04-17 23:49:59 +0800

    support staticfiles

--- qblog/settings.py
+++ qblog/settings.py
@@ -119,7 +119,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 # grappelli setting
 
@@ -127,7 +137,6 @@ GRAPPELLI_ADMIN_TITLE = "Gao's Blog"
 
 # ckeditor configuration
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 CKEDITOR_CONFIGS = {
     "awesome_ckeditor": {
         'toolbar': 'full',
